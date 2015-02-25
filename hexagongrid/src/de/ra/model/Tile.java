50b05ee5e115505bc453dc146a2821ef20976f8a
ramikaoko <sycorax.one@gmx.de>
2015-02-25 17:33:18 +0100

    basic widgets and panels implemented, more to come

--- hexagongrid/src/de/ra/model/Tile.java
+++ hexagongrid/src/de/ra/model/Tile.java
@@ -1,7 +1,59 @@
 package de.ra.model;
 
+import java.util.UUID;
+
 public class Tile {
 
-	
-	
+    private String mName;
+
+    private boolean isSpecial;
+
+    private boolean isHomeSystem;
+
+    private boolean isWormhole;
+
+    private int mQuality; // 1: good systems, 2: moderate systems, 3: bad systems
+
+    private UUID mId;
+
+    public Tile(String name, boolean special, boolean homeSystem, boolean wormhole, boolean quality)
+    {
+        setId(UUID.randomUUID());
+    }
+
+    public UUID getId()
+    {
+        return mId;
+    }
+
+    public void setId(UUID id)
+    {
+        mId = id;
+    }
+
+    public String getName()
+    {
+        return mName;
+    }
+
+    public boolean isSpecial()
+    {
+        return isSpecial;
+    }
+
+    public boolean isHomeSystem()
+    {
+        return isHomeSystem;
+    }
+
+    public boolean isWormhole()
+    {
+        return isWormhole;
+    }
+
+    public int getQuality()
+    {
+        return mQuality;
+    }
+
 }
--- hexagongrid/src/de/ra/view/Window.java
+++ hexagongrid/src/de/ra/view/Window.java
@@ -1,42 +1,92 @@
 package de.ra.view;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.SwingConstants;
 
 public class Window extends JFrame {
 
-	private JPanel contentPane;
+    private JPanel pnlOptions, pnlGrid;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Window frame = new Window();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    private JLabel lblHeader;
 
-	/**
-	 * Create the frame.
-	 */
-	public Window() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-	}
+    private JButton btnInfo;
 
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args)
+    {
+        EventQueue.invokeLater(new Runnable() {
+
+            public void run()
+            {
+                try
+                {
+                    Window frame = new Window();
+                    frame.setVisible(true);
+                } catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public Window()
+    {
+        setTitle("Mapmaker v0.1.03");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        createWidgets();
+        addWidgets();
+
+        setSize(1000, 600);
+        setLocation(800, 200);
+    }
+
+    private void addWidgets()
+    {
+        getContentPane().setLayout(null);
+        getContentPane().add(lblHeader);
+        getContentPane().add(btnInfo);
+        getContentPane().add(pnlOptions);
+        getContentPane().add(pnlGrid);
+    }
+
+    private void createWidgets()
+    {
+        // Header
+        lblHeader = new JLabel("TI3 Mapmaker");
+        lblHeader.setFont(lblHeader.getFont().deriveFont(Font.PLAIN, 11));
+        lblHeader.setForeground(Color.black);
+        lblHeader.setBackground(Color.white);
+        lblHeader.setHorizontalAlignment(SwingConstants.CENTER);
+        lblHeader.setBounds(0, 0, 1000, 20);
+
+        // Infobutton
+        btnInfo = new JButton("Info...");
+        btnInfo.setBounds(5, 530, 200, 25);
+
+        // Options panel
+        pnlOptions = new JPanel();
+        pnlOptions.setLayout(null);
+        pnlOptions.setBounds(5, 20, 200, 500);
+        pnlOptions.setBackground(Color.BLACK);
+
+        // Grid panel
+        pnlGrid = new JPanel();
+        pnlGrid.setLayout(null);
+        pnlGrid.setBounds(210, 20, 770, 500);
+        pnlGrid.setBackground(Color.BLACK);
+    }
 }
